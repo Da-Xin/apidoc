5acacc5a8832ba06781d2a0941c8cf7e11096bed
enhengs <daenheng@gmail.com>
2019-04-24 11:40:03 +0800

    Update route.php

--- src/route.php
+++ src/route.php
@@ -1,6 +1,6 @@
 <?php
-if (defined('THINK_VERSION') === false) {
-    Route::get('doc', "\\PhpApiDoc\\ApiDocSrc\\Doc@get");
+if (defined('THINK_VERSION') === false && class_exists('\think\facade\Route')) {
+    \think\facade\Route::get('doc', "\\PhpApiDoc\\ApiDocSrc\\Doc@get");
 } else {
     \think\Route::get('doc', "\\PhpApiDoc\\ApiDocSrc\\Doc@get");
 }
